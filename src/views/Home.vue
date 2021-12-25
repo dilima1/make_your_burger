d98ad9099b567dd5c67b26bb5c729023f7866115
Edilson Lima <edilson.lima1@hotmail.com>
2021-12-25 13:02:21 -0300

    CriandoFormularioCadastro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,20 @@
 <Banner />
   <div class="main-container">
     <h1>Monte seu burger:</h1>
-    <p>Formulário</p>
+    <BurgerForm />
 
   </div>
 </template>
 
 <script>
  import Banner from '../components/Banner.vue'
+ import BurgerForm from '../components/BurgerForm.vue'
 
  export default {
    name: "Home",
    components: {
-     Banner
+     Banner,
+     BurgerForm
    }
  }
 
