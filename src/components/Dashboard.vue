4f45b1d755b143e03cbff8939b16ff54fcd1acb1
Edilson Lima <edilson.lima1@hotmail.com>
2021-12-27 21:10:31 -0300

    Finalização-Projetos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
 <div id="burger-table">
+    <Message :msg="msg" v-show="msg" />
     <div>
         <div id="burger-table-heading">
             <div class="order-id">#:</div>
@@ -37,14 +38,20 @@
 </template>
 
 <script>
+
+import Message from './Message.vue'
 export default {
     name: "Dashboard",
     data() {
         return {
             burgers: null,
             burger_id: null,
-            status: []
+            status: [],
+            msg: null
         }
+    },
+    components: {
+        Message
     },
     methods: {
         async getPedidos() {
@@ -76,6 +83,10 @@ export default {
 
             const res = await req.json();
 
+            this.msg = `Pedido removido com sucesso!`;
+
+            setTimeout(() => this.msg = "", 3000);
+
             //msg
 
             this.getPedidos();
@@ -94,6 +105,13 @@ export default {
         });
 
         const res = await req.json();
+
+            //colocar uma msg de sistema
+            this.msg = `O pedido Nº ${res.id} foi atualizado para ${res.status}`;
+
+            // limpar msg
+            setTimeout(() => this.msg = "", 3000);
+
 
         console.log(res);
         
